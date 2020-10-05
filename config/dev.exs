22dfd924de846b4bf9a160f74035b01c75173b68
Rodrigo Compagnoni <rrrcompagnoni@gmail.com>
2020-10-04 22:59:19 -0300

    Reduce the number of clusters to three

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ config :libcluster,
   topologies: [
     project_y_cluster: [
       strategy: Cluster.Strategy.Epmd,
-      config: [hosts: [:"a@127.0.0.1", :"b@127.0.0.1", :"c@127.0.0.1", :"d@127.0.0.1"]],
+      config: [hosts: [:"a@127.0.0.1", :"b@127.0.0.1", :"c@127.0.0.1"]],
       connect: {:net_kernel, :connect_node, []},
       disconnect: {:erlang, :disconnect_node, []},
       list_nodes: {:erlang, :nodes, [:connected]}
